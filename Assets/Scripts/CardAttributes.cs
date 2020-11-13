e03179925f0b6006804a8eda3ac139f4e69c0f9f
Dieterism <diego_iory@hotmail.com>
2020-11-13 17:33:01 -0300

    Added Card GameObject and Enum class
    Added Enum script to access game enums instead of using raw values.

---
+++ Assets/Scripts/CardAttributes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+
+public class CardAttributes : MonoBehaviour
+{
+
+    private int cost = -1;
+    private CardType type = CardType.None;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enums
+{
+    public enum CardType
+    {
+        None = 0,
+        TypeElem1 = 1,
+        TypeElem2,
+        TypeElem3,
+        TypeElem4,
+        TypeElem5
+    }
+
+}

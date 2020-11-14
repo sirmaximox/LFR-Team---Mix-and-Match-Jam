1bc1a960c1425f29981b467170919272bf921489
Dieterism <diego_iory@hotmail.com>
2020-11-14 17:55:32 -0300

    Removed unused Card.prefab / Added GameInfo singleton
    Added game info singleton to control player life, deck list, player element type, etc.

---
+++ Assets/Scripts/Singleton/GameInfoSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class GameInfoSingleton : MonoBehaviour
+{
+
+    public static GameInfoSingleton Instance { get; private set; }
+
+    private int playerLife;
+    private CardElement currentPlayerType = CardElement.None;
+    //private List<CardInstance> deck = new List<CardInstance>;
+
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Player life functions
+    public int GetPlayerLife() => playerLife;
+    public void ModifyPlayerLife(int mod)
+    {
+        playerLife += mod;
+
+        //if (playerLife <= 0) GameFlowManager.triggerLose();
+    }
+
+    // Player Element functions
+    public CardElement GetCurrentPlayerElement() => currentPlayerType;
+    public void SetCurrentPlayerElement(CardElement newType) { currentPlayerType = newType; }
+}

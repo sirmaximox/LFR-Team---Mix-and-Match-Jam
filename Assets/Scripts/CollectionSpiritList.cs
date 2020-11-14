8c2e185d2583970e2aa3f216a5bb11b609902ef5
loipirius <62941931+loipirius@users.noreply.github.com>
2020-11-14 19:49:23 +0100

    Listas Cartas y Espiritus
    Listas de cartas y espiritus que se actualizan directamente desde el directorio
    
    Co-Authored-By: Dieterism <8959214+Dieterism@users.noreply.github.com>

---
+++ Assets/Scripts/CollectionSpiritList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[System.Serializable]
+public class CollectionSpiritList : MonoBehaviour
+{
+    public List<SpiritScriptable> spiritList = new List<SpiritScriptable>();
+    
+    private void Start()
+    {
+        String[] scriptableObjectArray = System.IO.Directory.GetFiles("Assets/Scripts/SpiritS");
+        String metaExtension = ".meta";
+        Match metaMatch;
+        foreach (var filename in scriptableObjectArray )
+        {
+            metaMatch = Regex.Match(filename, metaExtension);
+            if (!metaMatch.Success)
+            {
+                SpiritScriptable crd = AssetDatabase.LoadAssetAtPath(filename, typeof(SpiritScriptable )) as SpiritScriptable ;
+                spiritList.Add(crd);
+            }
+        }
+
+        foreach (var filename in spiritList )
+        {
+            Debug.Log(filename);
+        }
+
+    }
+}

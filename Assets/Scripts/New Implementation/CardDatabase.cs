46a221f5a40b9e0f9ec270ad2bbca7613ae5ca26
sirmaximox <74156784+sirmaximox@users.noreply.github.com>
2020-11-15 06:24:53 -0300

    Fixing bad fetchs

---
+++ Assets/Scripts/New Implementation/CardDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDatabase: MonoBehaviour
+{
+    public List<CardsScriptable> cardList;
+    public List<SpiritScriptable> spiritList;
+    public List<EnemyScriptable> enemyList;
+    public Dictionary<string, CardsScriptable> cardDictionary;
+
+    private static CardDatabase _instance;
+    public static CardDatabase Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("Card Database not found.");
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+
+    }
+
+
+}
---
+++ Assets/Scripts/New Implementation/FullCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class FullCard : MonoBehaviour
+{
+    [SerializeField]
+    private CardsScriptable card;
+    [SerializeField]
+    private SpiritScriptable spirit;
+
+    //constructor
+}
